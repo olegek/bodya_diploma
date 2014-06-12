716882c233d44f9f61a26e949b7f6e924cfd4d09
Hedgehog <hedgehog@MacBook-Pro-Hedgehog.local>
2014-06-12 14:36:08 +0300

    Initial Commit

---
+++ Bodya/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bodya
+//
+//  Created by Hedgehog on 12.06.14.
+//  Copyright (c) 2014 Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
